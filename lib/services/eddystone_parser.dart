5a853699be86c9ae915af75df6bff2f15849b786
Swayam Badhe <swayampbadhe@gmail.com>
2025-02-03 13:28:35 +0530

    feat: add initial EddystoneParser for identifying Eddystone beacons

---
+++ lib/services/eddystone_parser.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+
+class EddystoneParser {
+  static bool isEddystone(DiscoveredDevice device) {
+    return device.manufacturerData.isNotEmpty &&
+        device.manufacturerData.length >= 20 &&
+        device.manufacturerData[0] == 0xAA &&
+        device.manufacturerData[1] == 0xFE;
+  }
+}
